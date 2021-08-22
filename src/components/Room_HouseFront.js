496c94825442feebccbda00f410c25dcbeed73b9
hik-linux <nogueiralhsp@gmail.com>
2021-08-22 17:25:49 +0100

    garage and gardens lights on/off

--- src/components/Room_HouseFront.js
+++ src/components/Room_HouseFront.js
@@ -4,21 +4,56 @@ import {
 } from '../components'
 import { FaLightbulb } from 'react-icons/fa'
 
-export default function HouseFront() {
+class HouseFront extends React.Component {
 
-  return (
-    <div className="card_root">
-      <div className='card-title'>
-        <h2>House </h2>
+  constructor(props) {
+    super(props)
+    this.state = {
+      frontLightsOn: false,
+      driveWayLightsOn: false
+    }
+  }
+
+  render() {
+    return (
+      <div className="card_root">
+        <div className='card-title'>
+          <h2>House Outside</h2>
+        </div>
+        <div className='card-items'>
+          <Temperature_Control />
+        </div>
+        <div className='card-items'>
+          <h3>Lights</h3>
+          <a href=""
+            className='button'
+            onClick={(e)=>{
+              e.preventDefault()
+              this.setState((prevState) =>{
+                return {
+                  frontLightsOn:!this.state.frontLightsOn
+                }
+              })
+            }}
+          >
+            <FaLightbulb className={this.state.frontLightsOn ? 'iconStatusOn' : 'iconStatusOff'} />
+            {'\u00A0'}Front Door {this.state.frontLightsOn ? ' = On' : ' = Off'}</a>
+          <a href=""
+            className='button'
+            onClick={(e)=> {
+              e.preventDefault()
+              this.setState((prevState)=>{
+                return {
+                  driveWayLightsOn:!this.state.driveWayLightsOn
+                }
+              })
+            }}
+          >
+            <FaLightbulb className={this.state.driveWayLightsOn ? 'iconStatusOn' : 'iconStatusOff'} />
+            {'\u00A0'}Driveway {this.state.driveWayLightsOn ? ' = On' : ' = Off'}</a>
+        </div>
       </div>
-      <div className='card-items'>
-          <Temperature_Control/>
-      </div>
-      <div className='card-items'>
-        <h3>Lights</h3>
-        <a href="" className='button'><FaLightbulb className={true ? 'iconStatusOn' : 'iconStatusOff'} /> Front Door {true ? ' = On' : ' = Off'}</a>
-        <a href="" className='button'><FaLightbulb className={true === !true ? 'iconStatusOn' : 'iconStatusOff'} /> Driveway {true === !true ? ' = On' : ' = Off'}</a>
-      </div>
-    </div>
-  );
+    );
+  }
 }
+export default HouseFront
