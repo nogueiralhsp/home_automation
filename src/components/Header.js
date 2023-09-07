d7083218d337f5d77b94c7e245b2446c7ed4525c
Luiz Henrique Pinto <nogueiralhsp@gmail.com>
2023-09-07 17:28:28 +0100

    working around header and login_user

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,7 @@ import { LoginUser } from './index'
 
 const Header = () => (
      <header className="header">
-          <div className='header-logo'>
-               <h2>Home Automation Project</h2>
-               <p>72 Happy Street <br />Springfield</p>
+          <div>
           </div>
           <div>
                <LoginUser />
--- src/components/resorces/login_user.js
+++ src/components/resorces/login_user.js
@@ -11,13 +11,12 @@ class LoginUser extends React.Component {
     render() {
         return (
             <div className="login_area">
-                <h4 className="under_development">login area under development...</h4>
                 <input type="email" />
                 <input type="password" />
                 <div className="login_area_button">
                     <a href="" className="button_sign_in" type="submit">Sign In</a>
                 </div>
-                <h4>Don't have a user? Ok, navegate around and make contact if you have any doubt</h4>
+                
             </div>
         );
     }
